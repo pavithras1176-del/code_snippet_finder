7e9dd2b62936f59683eef2861cd630290b7597e7
Pavithra S <pavithras1176@gmail.com>
2026-03-05 12:49:27 +0530

    Improved Flask application structure

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ def home():
     return render_template("index.html",
                            results=results,
                            query=query)
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
